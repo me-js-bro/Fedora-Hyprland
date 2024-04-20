e05b72b60816e54ec433596207e301a5f8345606
me-js-bro <me.js.bro@gmail.com>
2024-04-20 14:57:38 +0600

    Removed kde-partitionmanager and added gparted, added script to install brave-browser

--- install-scripts/8-brave.sh
+++ install-scripts/8-brave.sh
@@ -34,100 +34,16 @@ log="Install-Logs/hypr_pkgs.log"
 ScrDir=`dirname "$(realpath "$0")"`
 source $ScrDir/1-global.sh
 
-# main packages
-hypr_package=( 
-  curl
-  firefox
-  git
-  gparted
-  grim
-  gwenview
-  jq
-  kitty
-  kvantum
-  libX11-devel
-  libXext-devel
-  lxappearance
-  make
-  network-manager-applet
-  neofetch
-  neovim
-  pamixer
-  pavucontrol
-  pipewire-alsa
-  polkit-gnome
-  python3-requests
-  python3-devel
-  python3-pip
-  qt5ct
-  qt6ct
-  qt6-qtsvg
-  rofi-wayland
-  slurp
-  swappy
-  tar
-  unzip
-  waybar
-  wget
-  wl-clipboard
-  wlogout
-  xdg-utils
-  yad
-)
-
-# these are optional, 
-hypr_package_2=(
-  # brightnessctl
-  btop
-  cava
-)
-
-# packages from the copr repo
-copr_packages=(
-  cliphist
-  pamixer
-  swaylock-effects
-  SwayNotificationCenter
-  swww
-)
-
-# thunar file manager
-thunar=(
-ffmpegthumbnailer
-file-roller
-gvfs
-gvfs-mtp 
-Thunar 
-thunar-volman 
-tumbler 
-thunar-archive-plugin
-)
-
-# url to install grimblast
-grimblast_url=https://github.com/hyprwm/contrib.git
-
-
-# Installation of main components
-printf "${action} - Installing hyprland packages.... \n"
-
-for PKG1 in "${hypr_package[@]}" "${hypr_package_2[@]}" "${copr_packages[@]}" "${thunar[@]}"; do
-  install_package "$PKG1" "$log"
-done
-
-# installation of grimblast
-if [ -f '/usr/local/bin/grimblast' ]; then
-  printf "${done} - Grimblast is already installed...\n"
-else
-
-  printf "${attention} - Cloning grimblast grom github to install for screenshot...\n"
-  git clone --depth=1 "$grimblast_url" ~/grimblast
-  cd "$HOME/grimblast/grimblast"
-  make
-  sudo make install
-
-  sleep 1
-  rm -rf ~/grimblast
-  printf "${done} - Grimblast was installed successfully"
-fi
-
+# Installing brave browser.
 clear
+
+printf "${action} - Now preparing to install ${cyan}Brave Browser${end}\n"
+sleep 1
+
+sudo dnf install -y dnf-plugins-core
+
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+
+sudo dnf install -y brave-browser
